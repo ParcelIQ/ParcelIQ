2b56bea10b025ac08deb94c01d43fad61754006f
Roman <roman.khadka13@gmail.com>
2025-04-28 00:25:00 -0600

    explicit import

--- lib/generators/tenant_model/tenant_model_generator.rb
+++ lib/generators/tenant_model/tenant_model_generator.rb
@@ -1,3 +1,6 @@
+require "rails/generators"
+require "rails/generators/named_base"
+
 class TenantModelGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("templates", __dir__)
   argument :attributes, type: :array, default: [], banner: "field:type field:type"
